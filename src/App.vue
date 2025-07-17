012240a53a94b8090223bedd125d1c40a21a6e6f
Maria <maria@paypoint.pro>
2025-07-18 00:28:47 +0300

    primevue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import InputText from 'primevue/inputtext';
 </script>
 
 <template>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <InputText />
 </template>
 
 <style scoped>
